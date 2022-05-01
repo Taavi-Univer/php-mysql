1858155aeec9c10e57d3112c94769d2e5ca9d3e6
Taavi-Univer <taavi.univer@gmail.com>
2022-05-01 20:12:30 +0300

    first commit

---
+++ year.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Year</title>
+</head>
+
+<body>
+    <?php
+    $username = "Taavi";
+    ?>
+    <p>It is: <?php echo date('M') ?></p>
+    <p>It is: <?php echo $username; ?></p>
+
+</body>
+
+</html>
