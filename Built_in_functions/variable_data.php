ba0729c1cc42ac8ca5e3e024d34b55a14b72b8e9
Taavi-Univer <taavi.univer@gmail.com>
2022-05-14 14:46:02 +0300

    var_dump

---
+++ Built_in_functions/variable_data.php
@@ -0,0 +1,45 @@
+<?php
+
+$username = 'Taavi';
+
+$user_array = [
+    'name' => 'Taavi',
+    'age' => 41,
+    'active' => true,
+];
+
+class User
+{
+    public $name;
+    public $age;
+    public $active;
+
+    public function __construct($name, $age, $active)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->active = $active;
+    }
+}
+
+$user_object = new User('Taavi', 41, true);
+$user_object2 = new User('Member', 51, false);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable Data</title>
+</head>
+
+<body>
+    <p>Variable: <?php var_dump($username); ?></p>
+    <p>Array: <?php var_dump($user_array); ?></p>
+    <p>Object: <?php var_dump($user_object); ?></p>
+    <p>Object2: <?php var_dump($user_object2); ?></p>
+</body>
+
+</html>
