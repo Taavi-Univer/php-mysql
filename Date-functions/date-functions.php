d0fd9e9586b3fc175f65ffa071ba9bb301242920
Taavi-Univer <taavi.univer@gmail.com>
2022-05-19 17:33:22 +0300

    updated date-functions.php

--- Date-functions/date-functions.php
+++ Date-functions/date-functions.php
@@ -1,11 +1,14 @@
 <?php
-$start = strtotime('May 19 2022');
+// $start = strtotime('May 19 2022');
 // var_dump($start);
-$end = mktime(0, 0, 0, 6, 1, 2022);
+// $end = mktime(0, 0, 0, 6, 1, 2022);
 
-$start_date = date('l, d M Y', $start);
-$end_date = date('l, d M Y', $end);
+// $start_date = date('l, d M Y', $start);
+// $end_date = date('l, d M Y', $end);
 
+
+$start = new DateTime('2022-05-19 17:25');
+$end = date_create_from_format('Y-m-d H:i', '2022-06-19 17:25');
 ?>
 
 <!DOCTYPE html>
@@ -18,8 +21,10 @@ $end_date = date('l, d M Y', $end);
 </head>
 
 <body>
-    <p>Sale starts: <?= $start_date ?></p>
-    <p>Sale ends: <?= $end_date ?></p>
+    <!-- <p>Sale starts: <?= $start_date ?></p>
+    <p>Sale ends: <?= $end_date ?></p> -->
+    <p>Sale starts: <?= $start->format('l, jS M Y H:i') ?></p>
+    <p>Sale ends: <?= $end->format('l, jS M Y') ?> at <?= $end->format('H:i') ?></p>
 </body>
 
 </html>
